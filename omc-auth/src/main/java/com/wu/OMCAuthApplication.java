6227073a34178dd2fb148d49139f0d3e6b6faea3
Masker <1262917629@qq.com>
2021-12-16 13:43:08 +0800

    添加权限模块，继续整理依赖文件

---
+++ omc-auth/src/main/java/com/wu/OMCAuthApplication.java
@@ -0,0 +1,13 @@
+package com.wu;
+
+/**
+ * Hello world!
+ *
+ */
+public class OMCAuthApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
