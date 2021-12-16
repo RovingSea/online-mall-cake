77e383dd52d428d16b8032bc2d1cc413deab3203
Masker <1262917629@qq.com>
2021-12-16 18:55:04 +0800

    添加user模块启动类

---
+++ omc-user/src/main/java/com/wu/user/OMCUserApplication.java
@@ -0,0 +1,19 @@
+package com.wu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户服务启动器
+ * @author Haixin Wu
+ * @date 2021/12/16
+ * @time 18:11
+ * @since 1.0
+ */
+@SpringBootApplication
+public class OMCUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OMCUserApplication.class, args);
+    }
+}
+
