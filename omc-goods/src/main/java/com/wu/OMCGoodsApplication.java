450748cd39d2fbdbfb9c572c371488e1bfa92beb
Masker <1262917629@qq.com>
2021-12-16 18:51:10 +0800

    新建商品服务模块

---
+++ omc-goods/src/main/java/com/wu/OMCGoodsApplication.java
@@ -0,0 +1,19 @@
+package com.wu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 商品服务启动器
+ * @author Haixin Wu
+ * @date 2021/12/16
+ * @time 18:10
+ * @since 1.0
+ */
+@SpringBootApplication
+public class OMCGoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OMCGoodsApplication.class, args);
+    }
+}
+
