9f3f226fae8cd0bc0f2fdbf05924efb729208724
Masker <1262917629@qq.com>
2021-12-16 20:01:03 +0800

    BaseMapper、GoodsMapper增删改查

--- omc-goods/src/main/java/com/wu/goods/repository/GoodsMapper.java
+++ omc-goods/src/main/java/com/wu/goods/repository/GoodsMapper.java
@@ -2,7 +2,7 @@ package com.wu.goods.repository;
 
 import com.wu.common.base.BaseMapper;
 import com.wu.common.domain.Goods;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 /**
  * @author Haixin Wu
@@ -13,15 +13,20 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface GoodsMapper extends BaseMapper<Goods> {
     @Override
+    @Delete("delete from onlinemallcake.goods where id = #{id}")
     boolean deleteByPrimaryKey(Integer id);
 
     @Override
+    @Insert("insert into onlinemallcake.goods(id, name, image1, image2, price, intro, stock, type_id)" +
+            "VALUES(default, name, image1, image2, price, intro, stock, type_id) ")
     boolean insert(Goods record);
 
     @Override
+    @Select("select * from onlinemallcake.goods where id = #{id}")
     Goods selectByPrimaryKey(Integer id);
 
     @Override
+    @Update("update onlinemallcake.goods set name = #{name}, image1 = #{image1}, image2 = #{image2}, price = #{price}, intro = #{intro}, stock = #{stock}, type_id = #{type_id}")
     boolean updateByPrimaryKey(Goods record);
 }
 
