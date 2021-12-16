f02868318f2e194b9b1d5dcc52d2dc23f2f56b6c
Masker <1262917629@qq.com>
2021-12-17 02:34:20 +0800

    引入工具类——JsonUtil；引入Json相关依赖

---
+++ omc-common/src/main/java/com/wu/common/utility/util/JsonUtil.java
@@ -0,0 +1,67 @@
+package com.wu.common.utility.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Json转换工具类
+ * @author Haixin Wu
+ * @date 2021/12/17
+ * @time 2:28 ^_^ 没错嗷，就是凌晨2.28分，此时室友都还没睡觉，无聊打开了Idea玩了玩，顺便把别人的Json工具类CV了过来
+ * @since 1.0
+ */
+public class JsonUtil {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+
+    static {
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static <T> String toJsonStr(T o) {
+        try {
+            return MAPPER.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static <T> T toJsonObject(String json, Class<T> valueType) {
+        try {
+            return MAPPER.<T>readValue(json, valueType);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+
+    public static <T> List<T> toJsonListObject(String json, Class<T> valueType) {
+        try {
+            JavaType getCollectionType = MAPPER.getTypeFactory().constructParametricType(List.class, valueType);
+            return MAPPER.readValue(json, getCollectionType);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static <T> T toJsonObject(InputStream stream, Class<T> valueType) {
+        try {
+            return MAPPER.<T>readValue(stream, valueType);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+}
+
