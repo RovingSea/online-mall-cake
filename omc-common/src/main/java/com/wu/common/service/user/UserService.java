4e51a4a0bf9124a8b94b7bb7d5ad493a067ed708
Masker <1262917629@qq.com>
2021-12-16 13:32:17 +0800

    初始化接口和用户模块实现类的包结构

---
+++ omc-common/src/main/java/com/wu/common/service/user/UserService.java
@@ -0,0 +1,14 @@
+package com.wu.common.service.user;
+
+/**
+ * 用户相关的接口
+ * @author Haixin Wu
+ * @date 2021/12/16
+ * @time 13:26
+ * @since 1.0
+ */
+public interface UserService {
+    boolean register();
+    boolean login();
+}
+
---
+++ omc-user/src/main/java/com/wu/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.wu.service.impl;
+
+/**
+ * @author Haixin Wu
+ * @date 2021/12/16
+ * @time 13:27
+ * @since 1.0
+ */
+public class UserServiceImpl {
+}
+
