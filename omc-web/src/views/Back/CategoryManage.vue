2c54a816ec138e959c70158f670aae258643ef9f
1024shao <3059680677@qq.com>
2021-12-16 22:18:22 +0800

    feat: 完成商品管理的静态展示

--- omc-web/src/views/Back/CategoryManage.vue
+++ omc-web/src/views/Back/CategoryManage.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="">
-    category
+  <div class="category">
+    <div class="search">
+      <input type="text"><button>添加类目</button>
+    </div>
   </div>
 </template>
 
@@ -14,4 +16,20 @@ export default defineComponent({
 </script>
 
 <style scoped lang="less">
+.category {
+  margin-top: 20px;
+
+  .search {
+    display: flex;
+    width: 700px;
+    margin-left: 100px;
+    input {
+      flex: 5;
+      margin-right: 10px;
+    }
+    button {
+      flex: 1;
+    }
+  }
+}
 </style>
--- omc-web/src/views/Goods/GoodsShow.vue
+++ omc-web/src/views/Goods/GoodsShow.vue
@@ -2,40 +2,61 @@
   <div class="goods-show">
     <div class="header">
       <ul>
-        <li>全部商品</li>
-        <li>条幅推荐</li>
-        <li>热销推荐</li>
-        <li>新品推荐</li>
+        <li> <img src="../../assets/images/cake1.png" alt=""> 全部商品</li>
+        <li> <img src="../../assets/images/cake2.png" alt=""> 条幅推荐</li>
+        <li> <img src="../../assets/images/cake3.png" alt=""> 热销推荐</li>
+        <li> <img src="../../assets/images/cake4.png" alt=""> 新品推荐</li>
       </ul>
 
     </div>
     <table>
-      <tr>
-        <th>ID</th>
-        <th>图片</th>
-        <th>名称</th>
-        <th>介绍</th>
-        <th>价格</th>
-        <th>种类</th>
-        <th>操作</th>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td><img src="https://picsum.photos/id/544/200/200" alt=""></td>
-        <td>芝士条</td>
-        <td>
-          。。。。。。。。。。。。。。。。
-        </td>
-        <td>28.0</td>
-        <td>儿童系列</td>
-        <td>
-          <button>移除条幅</button>
-          <button>移除热销</button>
-          <button>移除新品</button>
-          <button>删除</button>
-          <button>修改</button>
-        </td>
-      </tr>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>图片</th>
+          <th>名称</th>
+          <th>介绍</th>
+          <th>价格</th>
+          <th>种类</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>10</td>
+          <td><img src="https://picsum.photos/id/544/200/200" alt=""></td>
+          <td>芝士条</td>
+          <td>
+            。。。。。。。。。。。。。。。。
+          </td>
+          <td>28.0</td>
+          <td>儿童系列</td>
+          <td>
+            <div>移除条幅</div>
+            <div>移除热销</div>
+            <div>移除新品</div>
+            <div style="background-color: #e63326">删除</div>
+            <div style="background-color: #e6a726">修改</div>
+          </td>
+        </tr>
+        <tr>
+          <td>10</td>
+          <td><img src="https://picsum.photos/id/544/200/200" alt=""></td>
+          <td>芝士条</td>
+          <td>
+            。。。。。。。。。。。。。。。。
+          </td>
+          <td>28.0</td>
+          <td>儿童系列</td>
+          <td>
+            <div>移除条幅</div>
+            <div>移除热销</div>
+            <div>移除新品</div>
+            <div>删除</div>
+            <div>修改</div>
+          </td>
+        </tr>
+      </tbody>
     </table>
     <router-view></router-view>
   </div>
@@ -52,7 +73,7 @@ export default defineComponent({
 
 <style scoped lang="less">
 .goods-show {
-  margin: 40px 50px;
+  margin: 40px 50px 12px;
   width: 1200px;
   .header {
     height: 40px;
@@ -60,22 +81,43 @@ export default defineComponent({
     border-bottom: 5px dotted @fontColor;
   }
   ul {
-    width: 400px;
+    width: 700px;
     display: flex;
     li {
       text-align: center;
       flex: 1;
+      font-weight: 600;
+      color: @hoverColor;
+      cursor: pointer;
+      img {
+        width: 40px;
+        vertical-align: middle;
+      }
     }
   }
 
   table {
+    border: 3px solid rgb(133, 119, 75);
+    border-collapse: collapse;
     height: 500px;
     width: 100%;
     margin-top: 20px;
+    thead {
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 2px solid rgb(133, 119, 75);
+    }
+    tbody {
+      tr {
+        border: 1px solid rgb(133, 119, 75);
+        td {
+          border-right: 1px solid rgb(133, 119, 75);
+        }
+      }
+    }
     tr {
       display: flex;
       text-align: center;
-      height: 50px;
       td,
       th {
         width: 120px;
@@ -89,6 +131,17 @@ export default defineComponent({
         overflow: hidden;
         height: 200px;
         word-wrap: break-word;
+        color: @fontColor;
+        padding: 10px;
+        div {
+          margin: 0 auto 5px;
+          width: 80%;
+          background-color: @fontColor;
+          color: #fff;
+          padding: 5px;
+          margin-right: 4px;
+          cursor: pointer;
+        }
       }
     }
   }
