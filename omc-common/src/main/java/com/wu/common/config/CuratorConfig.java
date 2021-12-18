51d6d37c625f31d1cbb3513909fe4400c0918fa4
Masker <1262917629@qq.com>
2021-12-18 15:43:53 +0800

    增加分布式配置

---
+++ omc-common/src/main/java/com/wu/common/config/CuratorConfig.java
@@ -0,0 +1,42 @@
+package com.wu.common.config;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Haixin Wu
+ * @date 2021/12/18
+ * @time 14:59
+ * @since 1.0
+ */
+@Configuration
+public class CuratorConfig {
+    private final static String ZK_CLUSTER_ADDRESS = "39.96.61.59:2181,110.42.224.33:2181,180.76.136.123:2181";
+    private final static int MAX_CONNECTION_TIME_OUT = 50000;
+    private final static int MAX_SESSION_TIME_OUT = 10000;
+    private final static int MAX_RECONNECTION_TIME_OUT = 3000;
+    private final static int MAX_RETRIES = 3;
+
+
+    @Bean
+    public CuratorFramework curatorFramework(){
+        ExponentialBackoffRetry policy = new ExponentialBackoffRetry(MAX_RECONNECTION_TIME_OUT, MAX_RETRIES);
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                //主机
+                .connectString(ZK_CLUSTER_ADDRESS)
+                //连接超时
+                .connectionTimeoutMs(MAX_CONNECTION_TIME_OUT)
+                //会话超时
+                .sessionTimeoutMs(MAX_SESSION_TIME_OUT)
+                //连接失败后，间隔多久再重试
+                .retryPolicy(policy).build();
+        //启动客户端
+        client.start();
+
+        return client;
+    }
+}
+
---
+++ omc-common/src/main/java/com/wu/common/config/ZkReadWriteConfig.java
@@ -0,0 +1,39 @@
+package com.wu.common.config;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessReadWriteLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Zookeeper读写锁配置
+ * @author Haixin Wu
+ * @date 2021/12/18
+ * @time 15:08
+ * @since 1.0
+ */
+@Configuration
+public class ZkReadWriteConfig {
+
+    private static final String ZK_READ_WRITE_LOCATION_PATH = "/wu-omc-lock";
+
+    private final CuratorFramework zkCli;
+
+    @Autowired
+    public ZkReadWriteConfig(CuratorFramework zkCli) {
+        this.zkCli = zkCli;
+    }
+
+    @Bean
+    public InterProcessMutex readLock(){
+        return new InterProcessReadWriteLock(zkCli, ZK_READ_WRITE_LOCATION_PATH).readLock();
+    }
+
+    @Bean
+    public InterProcessMutex writeLock(){
+        return new InterProcessReadWriteLock(zkCli, ZK_READ_WRITE_LOCATION_PATH).writeLock();
+    }
+}
+
