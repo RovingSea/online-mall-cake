17e6255907ede714f8321dc4e2efe2d2b30f3d6b
Masker <1262917629@qq.com>
2021-12-18 18:07:27 +0800

    增加父类Controller

---
+++ omc-common/src/main/java/com/wu/common/base/BaseController.java
@@ -0,0 +1,13 @@
+package com.wu.common.base;
+
+/**
+ * 各模块Controller的祖先<br>
+ * 为各个Controller提供业务会用到的属性
+ * @author Haixin Wu
+ * @date 2021/12/18 17:27
+ * @since 1.0
+ */
+public class BaseController {
+
+}
+
