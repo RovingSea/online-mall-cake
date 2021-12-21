e0896005b1c3e6eae669af7ae6e6dba6b189295a
Masker <1262917629@qq.com>
2021-12-21 23:59:09 +0800

    重新整理分页查询

---
+++ omc-common/src/main/java/com/wu/common/model/GoodsPagingQueryByTypeModel.java
@@ -0,0 +1,36 @@
+package com.wu.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 分页查询模型
+ * @author Haixin Wu
+ * @date 2021/12/19 13:24
+ * @since 1.0
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GoodsPagingQueryByTypeModel implements Serializable {
+    /**
+     * 类型
+     */
+    private int typeId;
+    /**
+     * 从第几个数据开始
+     */
+    private int from;
+    /**
+     * 每页大小
+     */
+    private int eachPageSize;
+    /**
+     * 总数
+     */
+    private int amount;
+}
+
